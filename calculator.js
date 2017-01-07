e3e96fdbe3a4ed2ae5e3ed680d0bb1834c1ebc76
Mangku Widodo <mangkuwi26@gmail.com>
2017-01-08 03:01:27 +0700

    penjumlahan & pengurangan done

--- calculator.js
+++ calculator.js
@@ -1,8 +1,27 @@
 "use strict";
 
-export class Calculator {
+class Calculator {
   //write your code here
+  constructor(value = 1){
+    this._value = value
+  }
+  add(input){
+    this._value += input
+    return this
+  }
+  reduce(input){
+    this._value -= input
+    return this
+  }
+  result(){
+    return this._value
+  }
+
 }
+
+let execute = new Calculator()
+
+console.log(execute.add(8).divide(3).result());
 
 /** note : you can use several features from ecmascript, such as:
 * - Classes
