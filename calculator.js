6970b59f5909bd382405e11132f1ae9d72127b2b
Raditya <arya.raditya26@gmail.com>
2017-01-08 20:22:47 +0700

    bug di import & export

--- calculator.js
+++ calculator.js
@@ -2,6 +2,51 @@
 
 export class Calculator {
   //write your code here
+  constructor(input){
+    this.input = input
+  }
+
+  tambah(input){
+    this.input += input
+    return this
+  }
+
+  kurang(input){
+    this.input -= input
+    return this
+  }
+
+  kali(input){
+    this.input *= input
+    return this
+  }
+
+  bagi(input){
+    this.input /= input
+    return this
+  }
+
+  pangkat(input){
+    this.input = Math.pow(this.input, input)
+    return this
+  }
+
+  akar(input){
+    this.input = Math.sqrt(this.input, input)
+    return this
+  }
+
+  lingkaran(){
+    this.input = 3.14 * Math.pow(this.input, 2)
+    return this
+  }
+
+  hasil(){
+    console.log(this.input);
+    return this
+  }
+
+
 }
 
 /** note : you can use several features from ecmascript, such as:
--- main.js
+++ main.js
@@ -3,3 +3,10 @@
 import {Calculator} from "./calculator.js";
 
 // execute function on calculator.js in here
+let calculator = new Calculator(2)
+
+calculator.tambah(2).hasil().kali(6).hasil().kurang(4).hasil().bagi(2).hasil();
+
+let calculator2 = new Calculator(10)
+
+calculator2.pangkat(2).bagi(10).lingkaran().hasil()
