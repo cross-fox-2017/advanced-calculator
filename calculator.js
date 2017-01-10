e2fb528700440c7bea3a0b5ff8a61b4cb125c211
qblol <iqbal.0809@gmail.com>
2017-01-10 14:59:14 +0700

    done

--- calculator.js
+++ calculator.js
@@ -2,6 +2,40 @@
 
 export class Calculator {
   //write your code here
+  constructor(num) {
+    this.num = num
+  }
+
+  penjumlahan(num) {
+    this.num += num
+    return this
+  }
+
+  perkalian(num) {
+    this.num *= num
+    return this
+  }
+
+  pengurangan(num) {
+    this.num -= num
+    return this
+  }
+
+  pembagian(num) {
+    this.num /= num
+    return this
+  }
+
+  pangkat(num) {
+    this.num = Math.pow(this.num, num)
+    return this
+  }
+
+  akarPangkat(){
+    this.num = Math.sqrt(this.num)
+    return this
+  }
+
 }
 
 /** note : you can use several features from ecmascript, such as:
--- main.js
+++ main.js
@@ -3,3 +3,5 @@
 import {Calculator} from "./calculator.js";
 
 // execute function on calculator.js in here
+var cal = new Calculator(3)
+console.log(cal.penjumlahan(1).perkalian(2).pengurangan(-1).pembagian(3).pangkat(4).akarPangkat());
