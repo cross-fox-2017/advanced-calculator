254d1ae5f6d146370ad82bfd12d0d632f510931a
didietsuryadi <didietsuryadi@gmail.com>
2017-01-10 19:13:32 +0700

    calculator temp

--- calculator.js
+++ calculator.js
@@ -1,7 +1,31 @@
 "use strict";
 
 export class Calculator {
-  //write your code here
+  constructor(angka1 = 0){
+    this.nilai = angka1;
+  }
+
+  jumlah(angka2){
+    return this.nilai += angka2
+  }
+  kurang(angka3){
+    return this.nilai -= angka3
+  }
+  kali(angka4){
+    return this.nilai *= angka4
+  }
+  bagi(angka5){
+    return this.nilai /= angka5
+  }
+  pangkat(angka6){
+    return this.nilai = Math.pow(this.nilai, angka6);
+  }
+  akar(angka8){
+    return this.nilai = Math.sqrt(this.result);
+  }
+  lingkaran(angka9){
+    return this.nilai = Math.pow(this.result, 2) * 3.14
+  }
 }
 
 /** note : you can use several features from ecmascript, such as:
--- main.js
+++ main.js
@@ -2,4 +2,6 @@
 
 import {Calculator} from "./calculator.js";
 
+let calc = new Calculator(10)
+console.log(calc.jumlah(3).kurang(1).kali(5).bagi(2).pangkat(3).akar(5).lingkaran());
 // execute function on calculator.js in here
